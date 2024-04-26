3eb508721ced5a6443679c3047cbd5a7291731ac
Renan Pires <renan.sp.121@hotmail.com>
2024-04-26 17:58:29 -0300

    [26/04/2024]Renan Pires - Desafio 4 Completo.

---
+++ challenges_1to20/4Conversor_de_temperatura/main.py
@@ -0,0 +1,31 @@
+# Conversor de Temperatura: Crie um conversor de temperatura que converta entre Celsius, Fahrenheit e Kelvin.
+from temperature_converter import TemperatureConverter
+
+def main():
+    print("Bem-vindo ao Conversor de Temperatura!")
+
+    while True:
+        try:
+            temperature = input("\nDigite a temperatura (ou digite 'sair' para encerrar): ")
+
+            if temperature.lower() == "sair":
+                print("Obrigado por usar o Conversor de Temperatura. Adeus!")
+                break
+
+            temperature = float(input("\nDigite a temperatura: "))
+            scale = input("Digite a escala da temperatura (C - Celsius, F - Fahrenheit, K - Kelvin): ").strip().upper()
+
+            converter = TemperatureConverter(temperature, scale)
+
+            print("\nResultados:")
+            print("Celsius:", converter.to_celsius())
+            print("Fahrenheit:", converter.to_fahrenheit())
+            print("Kelvin:", converter.to_kelvin())
+
+        except ValueError:
+            print("Por favor, insira um valor numérico para a temperatura.")
+        except KeyError:
+            print("Escala não suportada. Por favor, use 'C', 'F' ou 'K'.")
+
+if __name__ == "__main__":
+    main()
